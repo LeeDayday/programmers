d7fb7fd18a0a3b580bf31e58827f0aa588e04556
LeeDayday <dayoon0905@gmail.com>
2024-05-10 23:12:31 +0900

    Update 크레인 인형뽑기 게임 solution

--- src/30-64061.py
+++ src/30-64061.py
@@ -1,50 +1,27 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/64061
 # 2019 카카오 개발자 겨울 인턴십
 
-# col_idx의 가장 꼭대기 인형 위치를 반환하는 함수
-# 꼭대기 인형 row 반환, 없으면 -1
-def last_doll(board, col_idx):
-    # 바닥 층에 인형이 없는 경우, 해당 열은 비어있음
-    if board[-1][col_idx] == 0:
-        return -1
-    else:
-        idx = len(board) - 1
-        while idx >= 0 and board[idx][col_idx] != 0:
-            idx -= 1
-        return idx + 1
-    
 
 def solution(board, moves):
     answer = 0
     stack = []
-    for move in moves:
-        #print(f"move: {move}")
-    
-        # move 번째 열에 인형이 있는지 확인
-        doll_row = last_doll(board, move-1)
-        #print(f"doll_row: {doll_row}")
-        if doll_row == -1:
-            continue
-        
-        # 인형 뽑기
-        
-        doll = board[doll_row][move-1]
-        #print(f"doll: {doll}")
-        board[doll_row][move-1] = 0
-        # 바구니가 비어있다면 인형 바로 넣기
-        if len(stack) == 0:
-            #print("바구니 empty")
-            stack.append(doll)
-        # 바구니가 차 있다면, 바구니의 top과 비교하기
-        else:
-            tmp_top = stack.pop() # top 임의로 pop
-            #print(f"{tmp_top}와 {doll} 비교")
-            # 인형이 동일하다면 answer 증가, 바구니에 넣지 않기
-            if doll == tmp_top:
-                answer += 2
-            # 동일하지 않다면 바구니에 넣기
-            else:
-                stack.append(tmp_top) # top 다시 넣기
-                stack.append(doll)
+
+    for i in moves:
+        for j in range(len(board)):
+            # 해당 column에 인형이 존재한다면 인형 뽑기 수행
+            if board[j][i-1] != 0:
+                # 인형 뽑기
+                stack.append(board[j][i-1])
+                # 보드에 인형 제거하기
+                board[j][i-1] = 0
+
+                # stack에 인형이 있는 경우, 꼭대기 인형과 비교
+                if len(stack) > 1:
+                    if stack[-1] == stack[-2]:
+                        stack.pop()
+                        stack.pop()
+                        answer += 2
+                break
+
         
     return answer
