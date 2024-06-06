2b82d721125d45f59148acdafe59243b89b8c2ab
LeeDayday <dayoon0905@gmail.com>
2024-06-06 22:02:13 +0900

    update 가장 많이 받은 선물 solution

--- src/30-258712.py
+++ src/30-258712.py
@@ -3,50 +3,25 @@
 
 
 def solution(friends, gifts):
-    answer = 0
-    friend_cnt = len(friends)
-    present = [[False for _ in range(friend_cnt + 1)] for _ in range(friend_cnt)] # 선물 지수를 표현한 이차원 배열
-    friend_dict = {} # 친구 이름과 idx를 관리하는 dictionary
-    for i, friend in enumerate(friends):
-        friend_dict[friend] = i
-        
-    for gift in gifts:
-        give, take = gift.split(" ")
-        give_idx = friend_dict.get(give)
-        take_idx = friend_dict.get(take)
-        if present[give_idx][take_idx] is False:
-            present[give_idx][take_idx] = 0
-            present[take_idx][give_idx] = 0
-        present[give_idx][take_idx] += 1
-        present[take_idx][give_idx] -= 1
-        
-    for i in range(friend_cnt):
-        cnt = 0 # 친구 별 선물 지수 계산
-        for j in range(friend_cnt):
-            if present[i][j] != False:
-                cnt += present[i][j]
-        present[i][-1] = cnt
-            
-    result = [0 for _ in range(friend_cnt)] 
-    for i in range(friend_cnt):
-        for j in range(friend_cnt):
-            if i == j:
-                continue
-            # 두 사람이 선물을 주고 받은 기록이 있다면
-            if present[i][j] != False and present[i][j] > 0:
-                # 더 많은 선물을 준 사람이 다음 달에 받는다
-                result[i] += 1
-                continue
-            # 주고 받은 기록이 없거나 주고받은 수가 같다면
-            if present[i][j] is False or present[i][j] == 0:
-                # 선물 지수가 더 큰 사람이 받는다
-                if present[i][-1] > present[j][-1]:
-                    result[i] += 1
-                # 선물 지수가 같은 경우 선물을 주고받지 않는다
-                elif present[i][-1] == present[j][-1]:
-                    continue
-                    
-    print(result)
-    answer = max(result)
+    f = {v: i for i, v in enumerate(friends)} # (이름-idx)
+    l = len(friends) # 전체 친구 수
+    p = [0] * l # 선물 지수
+    answer = [0] * l 
+    gr = [[0] * l for _ in range(l)] # 선물을 준 관계를 정리
     
-    return answer
+    for gift in gifts:
+        a, b = gift.split()
+        gr[f[a]][f[b]] += 1
+    
+    for i in range(l):
+        p[i] = sum(gr[i]) - sum([k[i] for k in gr])
+    
+    for i in range(l):
+        for j in range(l):
+            if gr[i][j] > gr[j][i]:
+                answer[i] += 1
+            elif gr[i][j] == gr[j][i]:
+                if p[i] > p[j]:
+                    answer[i] += 1
+                    
+    return max(answer)
