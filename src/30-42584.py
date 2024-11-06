1fb3471ccaa37cdd9aca60e12f31d4ba38ded1d2
LeeDayday <dayoon0905@gmail.com>
2024-11-06 16:24:34 +0900

    update 주식가격 solution

--- src/30-42584.py
+++ src/30-42584.py
@@ -1,20 +1,21 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/42584
 # 스택/큐
 
-from collections import deque
 def solution(prices):
-    answer = []
-    tmp = deque(prices)
-    for i in range(0, len(prices) - 1):
-        tmp.popleft()
-        cnt = 0
-        for num in tmp:
-            cnt += 1
-            if prices[i] > num:
-                break
-        answer.append(cnt)
-                
-        
-    answer.append(0)
+    n = len(prices)
+    answer = [0] * (n)
+    stack = [] # 가격이 떨어지지 않은 prices의 index
+    
+    for i in range(n):
+        # stack pop 여부 확인 (가격이 떨어졌는지 확인)
+        while stack and prices[stack[-1]] > prices[i]:
+            tmp = stack.pop()
+            answer[tmp] = i - tmp
+        stack.append(i)
+    
+    # 남은 stack 처리 (가격이 떨어진 적이 없는 index)
+    while stack:
+        tmp = stack.pop()
+        answer[tmp] = n - 1 - tmp
     
     return answer
