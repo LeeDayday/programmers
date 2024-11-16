a86c313dff65eb02a9e3bacc8036e0429bddbbcf
LeeDayday <dayoon0905@gmail.com>
2024-11-16 15:55:56 +0900

    update 구명보트 solution

--- src/30-42885.py
+++ src/30-42885.py
@@ -1,20 +1,15 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/42885
 # 탐욕법
 
-from collections import deque
-def solution(people, limit):
+def solution(people, limit) :
     answer = 0
-    # 보트 조건: 한 번에 최대 2명 / 최대 무게 제한: limit
     people.sort()
-    people = deque(people)
-    
-    while people:
-        # 무게 min + 무게 max 가 함께 탈 수 있는지 확인
-        if len(people) > 1 and people[0] + people[-1] <= limit:
-            people.pop()
-            people.popleft()
-        else: # 함께 탈 수 없는 경우, 무게 max 인원만
-            people.pop()
-        answer += 1
-            
-    return answer
+
+    a = 0 # 몸무게 min idx
+    b = len(people) - 1 # 몸무게 max idx
+    while a < b :
+        if people[b] + people[a] <= limit : # 두 사람이 탈 수 있는 경우
+            a += 1
+            answer += 1
+        b -= 1
+    return len(people) - answer # 전체 인원(모두 혼자 탄 경우) - 두 사람이 탄 경우
