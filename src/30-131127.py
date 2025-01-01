e302dd0d100006a4c7ccf21957a52ddc30253866
LeeDayday <dayoon0905@gmail.com>
2025-01-01 21:57:27 +0900

    update 할인 행사 solution

--- src/30-131127.py
+++ src/30-131127.py
@@ -1,5 +1,7 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/131127
+# https://school.programmers.co.kr/learn/courses/30/lessons/138476
 # 연습문제
+
+from collections import Counter
 
 def solution(want, number, discount):
     answer = 0
@@ -8,16 +10,7 @@ def solution(want, number, discount):
         data[want[i]] = number[i]
 
     for i in range(len(discount) - 9):
-        tmp_answer = 0
-        tmp_data = data.copy()
-        
-        for name in discount[i:i+10]:
-            if name in tmp_data.keys():
-                if tmp_data[name] > 0:
-                    tmp_data[name] -= 1
-                    tmp_answer += 1
-                    
-        if tmp_answer == 10:
+        if data == Counter(discount[i:i+10]):
             answer += 1
 
     return answer
