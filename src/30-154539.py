83e322c1e67d39a6478aaa39ba04865b10a91f1e
LeeDayday <dayoon0905@gmail.com>
2025-01-25 19:23:22 +0900

    update 뒤에 있는 큰 수 찾기 solution

--- src/30-154539.py
+++ src/30-154539.py
@@ -2,25 +2,11 @@
 # 연습문제
 
 def solution(numbers):
-    answer = [-1]
-    stack = [len(numbers) - 1]
-    for i in range(len(numbers) - 2, -1, -1):
-        if stack and numbers[i] < numbers[stack[-1]]:
-            answer.append(numbers[stack[-1]])
-            stack.append(i)
-        else:
-            if stack:
-                while stack:
-                    if numbers[i] < numbers[stack[-1]]:
-                        answer.append(numbers[stack[-1]])
-                        stack.append(i)
-                        break
-                    stack.pop()
-                else:
-                    answer.append(-1)
-                    stack.append(i)
-            else:
-                answer.append(-1)
-                stack.append(i)
+    answer = [-1] * len(numbers)
+    stack = []
+    for i in range(len(numbers)):
+        while stack and numbers[stack[-1]] < numbers[i]:
+            answer[stack.pop()] = numbers[i]
+        stack.append(i)
 
-    return answer[::-1]
+    return answer
