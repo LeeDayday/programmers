9fc3cd0f1ade8c30186918fb813c5c3c7d78f814
LeeDayday <dayoon0905@gmail.com>
2025-01-27 17:57:07 +0900

    add 숫자 변환하기 solution

---
+++ src/30-154538.py
@@ -0,0 +1,26 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/154538
+# 연습문제
+
+def solution(x, y, n):
+    answer = 0
+    s = set()
+    s.add(x)
+    
+    while s:
+        if y in s:
+            return answer
+        nx = set()
+        
+        for num in s:
+            if num + n <= y:
+                nx.add(num + n)
+            
+            if num * 2 <= y:
+                nx.add(num * 2)
+            
+            if num * 3 <= y:
+                nx.add(num * 3)
+        s = nx
+        answer += 1 
+    
+    return -1
