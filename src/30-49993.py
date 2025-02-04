fe3d4199ab23f61904d1101edac8e4c367455d8b
LeeDayday <dayoon0905@gmail.com>
2025-02-04 17:44:50 +0900

    update 스킬트리 solution

--- src/30-49993.py
+++ src/30-49993.py
@@ -1,21 +1,19 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/49993
 # Summer/Winter Coding(~2018)
 
+import re
+
 def solution(skill, skill_trees):
-    answer = 0    
-    skill_set = set(skill)
-    
+    answer = 0
+    pattern = f'[{skill}]'
     for data in skill_trees:
+        result = re.findall(pattern, data)
         s_idx = 0
-        for i in range(len(data)):
-            # 스킬트리 순서 만족
-            if s_idx < len(skill) and data[i] == skill[s_idx]:
-                s_idx += 1
-            else:
-                # 스킬트리 순서 위배
-                if data[i] in skill_set:
-                    break
+        for r in result:
+            if r != skill[s_idx]:
+                break
+            s_idx += 1
         else:
             answer += 1
-                    
+            
     return answer
