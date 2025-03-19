43cfb03e5280f991348f8b65b64947506e76e3d4
LeeDayday <dayoon0905@gmail.com>
2025-03-20 01:01:29 +0900

    review 무인도 여행 solution

--- src/review.py
+++ src/review.py
@@ -1,28 +1,40 @@
-# 복습 - 호텔 대실
-# https://school.programmers.co.kr/learn/courses/30/lessons/155651
+# 복습 - 무인도 여행
+# https://school.programmers.co.kr/learn/courses/30/lessons/154540
 
-# O(NlogN)
-from heapq import heappush, heappop
-def plus_ten_min(time):
-    h, m = map(int, time.split(":"))
-    m += 10
-    if m >= 60:
-        h += 1
-        m -= 60
-    return '{:02d}:{:02d}'.format(h, m)
-    
-def solution(book_time):
-    answer = 1
-    book_time.sort()
-    heap = [] # (체크 아웃 + 10분) 기준 힙 정렬
-    
-    for i in range(len(book_time)):
-        # 퇴실
-        while heap and heap[0] <= book_time[i][0]:
-            heappop(heap)
-        # 체크인
-        heappush(heap, plus_ten_min(book_time[i][1]))
-        answer = max(answer, len(heap))
+# O(NM + N log N) ≈ O(NM)
+from collections import deque
+
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+def solution(maps):
+    answer = []
+    n, m = len(maps), len(maps[0])
+    visited = [[False] * (m) for _ in range(n)]
+    def bfs(x, y):
+        result = 0
+        queue = deque([(x, y)])
+        visited[x][y] = True
         
-
+        while queue:
+            x, y = queue.popleft()
+            result += int(maps[x][y])
+            for i in range(4):
+                new_x = x + dx[i]
+                new_y = y + dy[i]
+                if 0 <= new_x < n and 0 <= new_y < m:
+                    if not visited[new_x][new_y] and maps[new_x][new_y] != 'X':
+                        queue.append((new_x, new_y))
+                        visited[new_x][new_y] = True
+        
+        return result
+        
+    for i in range(n):
+        for j in range(m):
+            if not visited[i][j] and maps[i][j] != 'X':
+                answer.append(bfs(i, j))
+                
+    if len(answer) == 0:
+        return [-1]
+    answer.sort()
+        
     return answer
