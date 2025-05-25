233f9770460a4b1a8db049db707d8d57c218ac85
LeeDayday <dayoon0905@gmail.com>
2025-05-25 16:24:11 +0900

    review 광물 캐기 solution

--- src/review.py
+++ src/review.py
@@ -1,29 +1,45 @@
-# 복습 - 프로세스
-# https://school.programmers.co.kr/learn/courses/30/lessons/42587
+# 복습 - 광물 캐기
+# https://school.programmers.co.kr/learn/courses/30/lessons/172927
 
-# O(NlogN)
+# O(3^n), n: 곡괭이 개수
 
-from collections import deque
+minerals_dict= {'diamond': 0, 'iron': 1, 'stone': 2}
+fatigue_dict = {"diamond": (1, 1, 1), "iron": (5, 1, 1), "stone": (25, 5, 1)}
 
-def solution(priorities, location):
-    answer = 0
-    queue = deque()
-    
-    for i in range(len(priorities)):
-        queue.append((priorities[i], i)) # (우선순위, 프로세스 위치)
-    
-    priorities.sort(reverse=True)
-    max_idx = 0
-    while queue:
-        # step 1    
-        curr_priority, curr_idx = queue.popleft()
+def dfs(picks, minerals, idx, fatigue):
+    global min_fatigue
 
-        # step 2
-        if curr_priority < priorities[answer]:
-            queue.append((curr_priority, curr_idx))
-        # step 3
-        else:
-            answer += 1
-            if curr_idx == location:
-                break             
-    return answer
+    # basecase: 모든 광물을 캤거나, 더 사용할 곡괭이가 없을 때 종료
+    if idx >= len(minerals) or sum(picks) == 0:
+        min_fatigue = min(min_fatigue, fatigue)
+        return
+
+    # 중간 종료 조건: 현재까지의 피로도가 최소 피로도보다 크다면 탐색 중단
+    if fatigue >= min_fatigue:
+        return
+
+    # 현재 위치에서 곡괭이 선택
+    for i, pick in enumerate(["diamond", "iron", "stone"]): # i: 곡괭이 index, pick: 곡괭이 종류
+        if picks[i] > 0:  # 해당 곡괭이가 남아있을 때만 사용 가능
+            picks[i] -= 1  # 곡괭이 사용
+
+            # 5개 단위로 광물 캐기
+            curr_fatigue = 0
+            for j in range(5):
+                if idx + j >= len(minerals):
+                    break
+                mineral = minerals[idx + j]
+                curr_fatigue += fatigue_dict[pick][minerals_dict[mineral]]
+
+            # 다음 탐색
+            dfs(picks, minerals, idx + 5, fatigue + curr_fatigue)
+            picks[i] += 1  
+
+def solution(picks, minerals):
+    global min_fatigue
+
+    min_fatigue = float('inf')
+
+    dfs(picks, minerals, 0, 0)  # 곡괭이 리스트, 광물 리스트, 광물 index, 누적 피로도
+
+    return min_fatigue
