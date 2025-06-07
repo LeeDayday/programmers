1c1f09dd0c7892c672bd9b99d99a2e7168d912ed
LeeDayday <dayoon0905@gmail.com>
2025-06-07 22:53:25 +0900

    add 영어 끝말잇기 solution

--- src/review.py
+++ src/review.py
@@ -1,23 +1,25 @@
-# 복습 - 마법의 엘리베이터
-# https://school.programmers.co.kr/learn/courses/30/lessons/148653
+# 복습 - 영어 끝말잇기
+# https://school.programmers.co.kr/learn/courses/30/lessons/12981
 
 # O(N)
 
-def solution(storey):
-    answer = 0
-    
-    while storey:
-        r = storey % 10
-        if r > 5:
-            storey += 10
-            answer += 10 - r
-        elif r < 5:
-            answer += r
-        # 5 인 경우, 다음 자리 수까지 고려
-        else:
-            if ((storey // 10) % 10) > 4:
-                storey += 10
-            answer += r
-        storey //= 10
-            
-    return answer
+from collections import defaultdict
+def solution(n, words):
+    result = defaultdict(list)
+    memory = set()
+    for idx, word in enumerate(words):
+        # 이미 사용한 단어를 또 사용한 경우
+        if word in memory:
+            break
+        # 끝말잇기가 이어지지 않은 경우
+        if idx != 0:
+            if result[(idx - 1) % n][-1][-1] != word[0]:
+                break
+        memory.add(word)
+        result[idx % n].append(word)
+    else:
+         return [0, 0]
+        
+    return [idx % n + 1, len(result[idx % n]) + 1]
+
+
