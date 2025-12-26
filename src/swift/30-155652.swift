029969f724f45136f26ee0f4538d79540f7adfa2
LeeDayday <dayoon0905@gmail.com>
2025-12-27 00:01:39 +0900

    add 둘만의 암호 solution

--- src/swift/30-155652.swift
+++ src/swift/30-155652.swift
@@ -4,23 +4,31 @@
 import Foundation
 
 func solution(_ s:String, _ skip:String, _ index:Int) -> String {
+    let skipSet: Set<Character> = Set(skip)
     let aValue: UInt8 = Character("a").asciiValue! // a의 ascii value
     let zValue: UInt8 = Character("z").asciiValue! // z의 ascii value
     
     var data: [Character] = [] // 변환가능한 알파벳
     var answer: String = ""
     
+    // 변환 가능한 알파벳 구하기
     for value in aValue...zValue {
-        if !skip.contains(Character(Unicode.Scalar(value))) {
-            data.append(Character(Unicode.Scalar(value)))
+        let ch = Character(Unicode.Scalar(Int(value))!)
+        if !skip.contains(ch) {
+            data.append(ch)
         }
+    }
+    
+    // 알파벳 별 인덱스 매핑하기
+    var alphaDict: [Character: Int] = [:] // 알파벳 별 인덱스
+    for (i, ch) in data.enumerated() {
+        alphaDict[ch] = i
     }
     
     let n: Int = data.count
     for ch in s {
-        let idx = data.firstIndex(of: ch)!
-        // print(data[(idx + index) % n], type(of: data[(idx + index) % n]))
-        answer += String(data[(idx + index) % n])
+        let idx = alphaDict[ch]!
+        answer.append(data[(idx + index) % n])
     }
     
     
