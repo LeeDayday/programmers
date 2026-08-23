3e73526673280218080f166c0a64b75d07f6b6f7
Lee Dayoon <95182422+LeeDayday@users.noreply.github.com>
2026-08-23 22:21:52 +0900

    Update 타겟 넘버 solution

--- src/python/30-43165.py
+++ src/python/30-43165.py
@@ -2,17 +2,13 @@
 # 깊이/너비 우선 탐색 (DFS/BFS)
 
 def solution(numbers, target):
-    answer = [0]
-    def dfs(idx, result): # idx: 접근할 numbers의 index, result: 누적값
-        if idx == len(numbers):
-            if result == target:
-                answer[0] += 1
-                return
-            else:
-                return
-        dfs(idx + 1, result + numbers[idx])
-        dfs(idx + 1, result - numbers[idx])
-        return
+    n = len(numbers)
+    def dfs(idx, total):
+        # basecase: 모두 순회한 경우
+        if idx == n:
+            return 1 if total == target else 0
+        
+        return dfs(idx + 1, total + numbers[idx]) + dfs(idx + 1, total - numbers[idx])
     
-    dfs(0, 0)
-    return answer[0]
+    
+    return dfs(0, 0)
